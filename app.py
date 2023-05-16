0ba86acc6cd89103e28c13f113f5d3a604229b7d
Matt Majestic <33904170+mattmajestic@users.noreply.github.com>
2023-05-16 12:02:12 +0000

    update to abhi data

--- app.py
+++ app.py
@@ -12,37 +12,34 @@ st.write("Analysis of player performance at Georgia Tech soccer games")
 r = requests.get(f'https://docs.google.com/spreadsheet/ccc?key=1tPCnVRoxTh597qqxCGqZ5PpFVw8JhvJW1ODo0jH9lA0&output=csv')
 open('dataset.csv', 'wb').write(r.content)
 df = pd.read_csv('dataset.csv')
-df['Date'] = pd.to_datetime(df['Date'])
 
-# Load data
-# df = pd.read_csv("tech-stats.csv")
+# Remove rownames
+df = df.rename(columns={'Unnamed: 0': ''})
+df.set_index('', inplace=True)
 
-# Compute derived data
-df["Goals_per_game"] = df["Goals"] / df.groupby("Player")["Date"].transform("nunique")
-df["Assists_per_game"] = df["Assists"] / df.groupby("Player")["Date"].transform("nunique")
-total_goals = df.groupby("Player")["Goals"].sum()
-total_assists = df.groupby("Player")["Assists"].sum()
+# Calculate metrics
+df['Goals per Game'] = df['Goals'] / df['Games Played']
+df['Assists per Game'] = df['Assists'] / df['Games Played']
+df['G/A per Game'] = (df['Goals'] + df['Assists']) / df['Games Played']
+
+# Sort the dataframes
+goals_per_game_df = df[['Player Name', 'Goals per Game']].sort_values(by='Goals per Game', ascending=False)
+assists_per_game_df = df[['Player Name', 'Assists per Game']].sort_values(by='Assists per Game', ascending=False)
+total_goals_df = df[['Player Name', 'Goals']].sort_values(by='Goals', ascending=False)
+total_assists_df = df[['Player Name', 'Assists']].sort_values(by='Assists', ascending=False)
 
 # Display tables
-st.write("## Per Game Goals and Assists")
-per_game_table = pd.DataFrame({
-    "Player": df["Player"].unique(),
-    "Goals per Game": df.groupby("Player")["Goals_per_game"].mean(),
-    "Assists per Game": df.groupby("Player")["Assists_per_game"].mean()
-})
-per_game_table = per_game_table.sort_values("Goals per Game", ascending=False)  # Sort by Goals per Game
-per_game_table.reset_index(drop=True, inplace=True) 
-st.table(per_game_table)
+st.write("## Goals per Game")
+st.table(goals_per_game_df)
 
-st.write("## Total Goals and Assists")
-total_table = pd.DataFrame({
-    "Player": total_goals.index,
-    "Total Goals": total_goals,
-    "Total Assists": total_assists
-})
-total_table = total_table.sort_values("Total Goals", ascending=False)  # Sort by Total Goals
-total_table.reset_index(drop=True, inplace=True)
-st.table(total_table)
+st.write("## Assists per Game")
+st.table(assists_per_game_df)
+
+st.write("## Total Goals")
+st.table(total_goals_df)
+
+st.write("## Total Assists")
+st.table(total_assists_df)
 
 # Display video
 st.write("")
